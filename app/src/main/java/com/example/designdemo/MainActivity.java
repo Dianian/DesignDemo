af9c555b734d5763019a2530b8853a4b8234f375
boo <fuboowen@gmail.com>
2017-12-04 17:10:24 +0800

    RefreshLayout

--- app/src/main/java/com/example/designdemo/MainActivity.java
+++ app/src/main/java/com/example/designdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -30,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private ImgAdapter mImgAdapter;
     private RecyclerView mListRe;
 
-    private List<Img> mImgs=new ArrayList<>();
+    private List<Img> mImgs = new ArrayList<>();
+    private SwipeRefreshLayout mRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,6 +93,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mListRe = findViewById(R.id.list_re);
+
+        mRefresh = findViewById(R.id.refresh);
+        mRefresh.setColorSchemeResources(R.color.colorPrimary);//设置下拉刷新的颜色
+        mRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getData();
+                mImgAdapter.notifyDataSetChanged();
+                mRefresh.setRefreshing(false);
+            }
+        });
     }
 
     private void initAdapter() {
